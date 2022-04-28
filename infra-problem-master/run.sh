3a27f060d34cd9c82b76bdce8b4024866dcf2d29
Shawal Ahmad Mohmand <criticalmiind@gmail.com>
2022-04-29 02:05:25 +0500

    changes

--- infra-problem-master/run.sh
+++ infra-problem-master/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-java -jar ./build/quotes.jar;
-java -jar ./build/newsfeed.jar;
-java -jar ./build/front-end.jar;
+java -jar ./build/quotes.jar &;
+java -jar ./build/newsfeed.jar &;
+java -jar ./build/front-end.jar &;
